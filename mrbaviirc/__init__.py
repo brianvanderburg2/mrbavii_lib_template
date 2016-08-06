a1c6439333ac86cb8bbc666de49ab1ed5c41b9e0
Brian Allen Vanderburg II <brianvanderburg@users.sourceforge.net>
2016-08-06 08:11:22 -0400

    setup.py read metadata from __init__.py

--- mrbaviirc/__init__.py
+++ mrbaviirc/__init__.py
@@ -1,8 +1,10 @@
 """ A collection of reusable code modules. """
 
-__version__     = "0.0.1"
+__version__     = "0.1.2"
 __author__      = "Brian Allen Vanderburg II"
 __copyright__   = "Copyright 2016"
 __license__     = "Apache License 2.0"
 
+# There should be no direct imports from this packages module so the
+# setup system can import and read the above metadata
 
